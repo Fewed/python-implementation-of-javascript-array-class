195f70f14a4c47b47d64b18bda2b985ec3db71f9
Mikhail Sementsov <fewebdev@gmail.com>
2019-08-30 21:09:05 +0300

    join fix

--- arr.py
+++ arr.py
@@ -20,8 +20,49 @@ class Array:
             new_arr.value += [res]
         return new_arr
 
-    def join(self, separator=''):
+    def filter(self, cb):
+        argcount = cb.__code__.co_argcount
+        new_arr = self.__duplicate([])
+
+        for idx, item in enumerate(self.value):
+            arg_list = (item, idx, self.value)[:argcount]
+            if cb(*arg_list):
+                new_arr.value += [item]
+
+        return new_arr
+
+    def reduce(self, cb, initial):
+        argcount = cb.__code__.co_argcount
+        temp = initial
+
+        for idx, item in enumerate(self.value):
+            arg_list = (temp, item, idx, self.value)[:argcount]
+            temp = cb(*arg_list)
+
+        return temp
+
+    def join(self, separator=','):
+        print(separator, len(separator))
         temp = ''
         for item in self.value:
             temp += str(item) + separator
-        return temp[:-len(separator)]
+        if len(separator):
+            return temp[:-len(separator)]
+        return temp
+
+    def push(self, *args):
+        self.value += args
+        return len(self.value)
+
+    def pop(self):
+        temp = self.value[-1]
+        self.value = self.value[:-1]
+        return temp
+
+    def slice(self, start=0, end=-1):
+        return self.__duplicate(self.value[start:end])
+
+    def splice(self, start, delete_count, *items):
+        deleted = self.value[start:start + delete_count]
+        self.value = self.value[:start] + list(items) + self.value[start + delete_count:]
+        return deleted
--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from arr import *
 arr = Array(1, 2, 7, 3, 5)
 a = arr
 b = arr.reverse().map(lambda x: x ** 2)
-c = b.join('-')
+c = b.join('')
+d = a.filter(lambda x: x % 2)
+e = a.reduce(lambda acc, cur: acc + cur, 0)
+f = a.slice(1, -2)
+g = a.splice(1, 1, 42)
 
 print(a)
 print(a.value)
 print(b.value)
 print(c)
+print(d.value)
+print(e)
+print(f.value)
+print(g)
